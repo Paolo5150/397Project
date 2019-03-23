f8d63e2ffeba9f3ae158be7c36fca018fc32a18f
Dylan Green <Dylan@thefunkygibbon.com>
2019-03-23 18:09:05 +0800

    Input Update
    Input:
    +Input now does something
    +Added Init to initialise callbacks
    +Added keys variable to store key states
    +Added Doxy Comments
    -Removed private GLFWwindow variable

--- Engine/src/Core/Input.cpp
+++ Engine/src/Core/Input.cpp
@@ -1,33 +1,80 @@
 #include "..\pch.h"
 #include "Input.h"
 
+//-----Public-----//
+void Input::Init(GLFWwindow* window, bool logErrors)
+{
+	glfwSetKeyCallback(window, Key_Callback);
+
+	if (logErrors == true)
+	{
+		glfwSetErrorCallback(Error_Callback);
+	}
+
+	for (int i = 0; i < 400; i++)
+	{
+		keys[i] = 0;
+	}
+}
 
 void Input::Update()
 {
 	glfwPollEvents();
 }
 
-bool Input::GetKeyPressed()
+bool Input::GetKeyPressed(int key)
 {
-
+	if (keys[key] == GLFW_PRESS)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
-bool Input::GetKeyDown()
+bool Input::GetKeyDown(int key)
 {
-
+	if (keys[key] == GLFW_REPEAT)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
-bool Input::GetKeyUp()
+bool Input::GetKeyUp(int key)
 {
-
+	if (keys[key] == GLFW_RELEASE)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
-void Input::SetWindow(GLFWwindow* window)
+
+//-----Private-----//
+int Input::keys[400];
+
+void Input::Key_Callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	_window = window;
+	if (key >= 0 && key < 400)
+	{
+		keys[key] = action;
+	}
+	else
+	{
+		Logger::LogError("Key_Callback went out of range!");
+	}
 }
 
-GLFWwindow* Input::GetWindow() const
+void Input::Error_Callback(int error, const char* description)
 {
-	return _window;
+	Logger::LogError(description);
 }
--- Engine/src/Core/Input.h
+++ Engine/src/Core/Input.h
@@ -1,17 +1,87 @@
 #pragma once
 #include <GLFW\glfw3.h>
+#include "Logger.h"
 
+/**
+* @class Input
+* @brief Allows for retreiving of GLFW keystates
+*
+*
+*Contains functions to determine the current state of keys
+*
+* @author Dylan Green
+* @version 01
+* @date 15/03/2018
+*
+*
+* @bug No known bugs.
+*/
 static class Input
 {
 public:
-	void Update();
-	bool GetKeyPressed(GLFWwindow* window, int key);
-	bool GetKeyDown(GLFWwindow* window, int key);
-	bool GetKeyUp();
-	void SetWindow(GLFWwindow* window);
-	GLFWwindow* GetWindow() const;
+//---- Public Member Functions ----//
+	//-- GLFW --//
+	/**
+	* @brief		Initialises the glfwSetKeyCallback and keys array, and optionally the glfwSetErrorCallback
+	*
+	* @pre			GLFW must be initialised
+	* @post			glfwSetKeyCallback is intialised in reference to the window input
+	*
+	* @param		window		GLFWwindow pointer to reference for the KeyCallback
+	* @param		logErrors	Enables glfwSetErrorCallback, which logs glfw errors to the Logger
+	*/
+	static void Init(GLFWwindow* window, bool logErrors = false);
+
+	/**
+	* @brief		Facade that calls glfwPollEvents(), this should be called regularly
+	*/
+	static void Update();
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	//-- Accessors --//
+	/**
+	* @brief		Returns whether the key specified equals GLFW_PRESS
+	*
+	* @pre			The GameObject must exist.
+	* @post			The true or false, depending on the value stored in keys[key] will be retreived
+	*
+	* @return		true if keys[key] equals GLFW_PRESS, else false
+	*/
+	static bool GetKeyPressed(int key);
+
+	/**
+	* @brief		Returns whether the key specified equals GLFW_REPEAT
+	*
+	* @pre			The GameObject must exist.
+	* @post			The true or false, depending on the value stored in keys[key] will be retreived
+	*
+	* @return		true if keys[key] equals GLFW_REPEAT, else false
+	*/
+	static bool GetKeyDown(int key);
+
+	/**
+	* @brief		Returns whether the key specified equals GLFW_RELEASE
+	*
+	* @pre			The GameObject must exist.
+	* @post			The true or false, depending on the value stored in keys[key] will be retreived
+	*
+	* @return		true if keys[key] equals GLFW_RELEASE, else false
+	*/
+	static bool GetKeyUp(int key);
 
 private:
-	GLFWwindow* _window;
+	static int keys[400]; //Stores the last updated value of the keys
+
+//---- Private Member Functions ----//
+	//-- GLFW Callbacks --//
+	/**
+	* @brief		Callback for glfwSetKeyCallback
+	*/
+	static void Key_Callback(GLFWwindow* window, int, int, int, int);
+
+	/**
+	* @brief		Callback for glfwSetErrorCallback
+	*/
+	static void Error_Callback(int error, const char* description);
 };
 
