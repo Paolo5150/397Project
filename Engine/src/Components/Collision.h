a57f5863008a0c7d25d073fc9a7bee8b296f0e2a
Conean <eggcrack9@gmail.com>
2019-04-13 19:27:29 +0800

    Sphere Collider code
    Sphere Collider Code

---
+++ Engine/src/Components/Collision.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "../GameObject/Component.h"
+#include "../Core/Transform.h"
+#include "MeshRenderer.h"
+#include "../GameObject/GameObject.h"
+#include "../Utils/AssetLoader.h"
+
+class Collider : public Component
+{
+	public:
+		Collider(std::string name) : Component(name){}
+		Transform trans;
+		MeshRenderer* mr;
+		Material mat;
+};
+
+class SphereCollider : public Collider
+{
+	public:
+		SphereCollider();
+		void Update() override { mr->Update(); }
+		bool checkCollision(SphereCollider);
+
+};
