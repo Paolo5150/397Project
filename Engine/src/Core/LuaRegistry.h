174e08c19dfa756f33ccffc213d379f18c2d0617
Dylan Green <Dylan@thefunkygibbon.com>
2019-04-17 14:59:18 +0800

    Documentation
    Lua:
    ~Fixed tabbing on documentation
    
    LuaRegistry:
    +Added documentation
    
    Component:
    +Added documentation
    
    GameObject:
    ~Fixed tabbing and spacing

--- Engine/src/Core/LuaRegistry.h
+++ Engine/src/Core/LuaRegistry.h
@@ -6,9 +6,30 @@
 #include "..\Utils\Asset.h"
 #include "..\Utils\GameAssetFactory.h"
 
+/**
+* @class Lua
+* @brief Contains all of the c++ function wrappers for use in lua scripts
+*
+* @see Lua#RegisterCppFunctions
+*
+* @author Dylan Green
+* @version 01
+* @date 11/04/2019
+*
+*
+* @bug No known bugs.
+*/
 static class LuaRegistry
 {
 public:
+//---- Public Member Functions ----//
+	/**
+	* @brief		Wrapper for GameAssetFactory.Create()
+	*
+	* @param		L lua state for lua cfunction
+	*
+	* @see			GameAssetFactory#Create
+	*/
 	static int LuaRegistry::Lua_Create(lua_State* L);
 };
 
