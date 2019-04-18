0735fec5269327d2b58f8d1351f4ff4292576528
Paolo86 <p.ferri1986@gmail.com>
2019-04-18 22:58:47 +0800

    Added mitch's pic

--- Application/src/Scenes/ExitScene.cpp
+++ Application/src/Scenes/ExitScene.cpp
@@ -21,6 +21,8 @@ void ExitScene::LoadAssets() {
 
 	AssetLoader::Instance().LoadTexture("Assets\\Textures\\paolo.png");
 	AssetLoader::Instance().LoadTexture("Assets\\Textures\\dylan.png");
+	AssetLoader::Instance().LoadTexture("Assets\\Textures\\mitch.png");
+
 
 
 }
@@ -37,9 +39,12 @@ void ExitScene::Initialize() {
 
 	 GUIManager::Instance().AddGUIObject(new GUIImage(AssetLoader::Instance().GetAsset<Texture2D>("paolo"), 10, 20, 25, 35, true)); 
 	 GUIManager::Instance().AddGUIObject(new GUIImage(AssetLoader::Instance().GetAsset<Texture2D>("dylan"), 40, 20, 25, 35, true));
+	 GUIManager::Instance().AddGUIObject(new GUIImage(AssetLoader::Instance().GetAsset<Texture2D>("mitch"), 70, 20, 25, 35, true));
 
 	 GUIManager::Instance().AddGUIObject(new GUIText("Paolo Ferri", 10, 60, 0.1f, 0.1f, 0.1f, true));
 	 GUIManager::Instance().AddGUIObject(new GUIText("Dylan Green", 40, 60, 0.1f, 0.1f, 0.1f, true));
+	 GUIManager::Instance().AddGUIObject(new GUIText("Mitchell Hollings", 70, 60, 0.1f, 0.1f, 0.1f, true));
+
 
 
 }
