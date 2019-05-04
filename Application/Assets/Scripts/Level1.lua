7be73e926713a65f74d9599ed6180a78e790e3c7
Dylan Green <Dylan@thefunkygibbon.com>
2019-05-04 14:21:56 +0800

    Adding transforms to Lua_functions
    LuaRegistry:
    +Added position, scale, and rotation to CreateAsset/Object
    
    Level1.lua:
    ~Switching old CreateAsset calls to new format

--- Application/Assets/Scripts/Level1.lua
+++ Application/Assets/Scripts/Level1.lua
@@ -1,121 +1,89 @@
-CreateAsset("MainCamera");
-camX, camY, camZ = 0, 35, 0;
+CreateAsset("MainCamera", 0, 35, 0, 30, 180, 0);
 camRotX, camRotY, camRotZ = 30, 180, 0;
 
-CreateAsset("Water");
-waterX, waterY, waterZ = 0, 100, 0;
 waterScaleX, waterScaleY, waterScaleZ = 2000, 2000, 1;
+CreateAsset("Water", 0, 100, 0, waterScaleX, waterScaleY, waterScaleZ);
 
 --Create all npcs and props
+nanosuitScale = 2;
+CreateObject("Model", "Nanosuit", 1900, 0, 1900, nanosuitScale, nanosuitScale, nanosuitScale);
+CreateObject("Model", "Nanosuit", 1700, 0, 2300, nanosuitScale, nanosuitScale, nanosuitScale);
 
+pumpkinScale = 20;
+CreateAsset("Model", "Pumpkin", 2234, 0, 1833, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 255, 0, 1007, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 1043, 0, 2660, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 1558, 0, 2772, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 1628, 0, 1189, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 580, 0, 2778, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 277, 0, 2515, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 1330, 0, 2158, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 276, 0, 1642, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 270, 0, 359, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 2125, 0, 1724, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 2320, 0, 2302, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 277, 0, 1477, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 118, 0, 427, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 1589, 0, 2115, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 360, 0, 2176, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 2617, 0, 1129, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 242, 0, 2715, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 209, 0, 774, pumpkinScale, pumpkinScale, pumpkinScale);
+CreateAsset("Model", "Pumpkin", 219, 0, 874, pumpkinScale, pumpkinScale, pumpkinScale);
 
-npc_nanosuits = 2;
-npc_pumpkins = 10;
-prop_barrels = 10;
-prop_crates = 10;
+barrelScale = 20;
+CreateAsset("Model", "Barrel", 1116, 0, 1327, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 1116, 0, 1327, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 1289, 0, 1066, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 1325, 0, 1826, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 253, 0, 237, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 649, 0, 1589, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 731, 0, 2747, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 1194, 0, 1025, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 43, 0, 672, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 1363, 0, 1678, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 955, 0, 2148, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 608, 0, 2287, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 101, 0, 803, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 110, 0, 792, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 717, 0, 1703, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 231, 0, 928, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 1590, 0, 1331, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 199, 0, 501, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 79, 0, 919, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 1202, 0, 1652, barrelScale, barrelScale, barrelScale);
+CreateAsset("Model", "Barrel", 1431, 0, 1566, barrelScale, barrelScale, barrelScale);
 
-asset_total = npc_nanosuits + npc_pumpkins + prop_barrels;
+crateScale = 1;
+CreateAsset("Model", "Crate", 1633, 0, 1555, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 220, 0, 1613, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 220, 0, 2470, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 2290, 0, 622, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 681, 0, 2505, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 1626, 0, 1778, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 831, 0, 2193, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 1240, 0, 1871, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 1359, 0, 962, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 1085, 0, 1347, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 2401, 0, 217, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 771, 0, 1333, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 1411, 0, 1539, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 1818, 0, 2552, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 2794, 0, 524, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 1706, 0, 1023, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 1217, 0, 2102, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 804, 0, 1014, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 1135, 0, 275, crateScale, crateScale, crateScale);
+CreateAsset("Model", "Crate", 1708, 0, 1886, crateScale, crateScale, crateScale);
 
-for i = 0, npc_nanosuits-1, 1
-do
-    CreateAsset("Model", "Nanosuit");
-end
-    nanosuitScale = 2;
+spiderScale = 1;
+CreateObject("Spider", 80, 0, 1298, spiderScale, spiderScale, spiderScale);
 
-for i = 0, npc_pumpkins-1, 1
-do
-    CreateAsset("Model", "Pumpkin");
-end
-    pumpkinScale = 20;
-
-for i = 0, prop_barrels-1, 1
-do
-    CreateAsset("Model", "Barrel");
-end
-    barrelScale = 20;
-
-for i = 0, prop_crates-1, 1
-do
-    CreateAsset("Model", "Crate");
-end
-    crateScale = 1;
-
-CreateObject("Spider", 80, 0, 1298, 1)
-
-CreateAsset("Model", "Gun");
-gunX, gunY, gunZ = 1637, 16, 1555; --Same x and z location as crate 1, y = 16 is to lay the gun on a crate
 gunScale = 0.8;
+CreateAsset("Model", "Gun", 1637, 16, 1555, gunScale, gunScale, gunScale); --Same x and z location as crate 1, y = 16 is to lay the gun on a crate
 
-CreateAsset("Model", "Ship");
-shipX, shipY, shipZ = 1545, 0, 1414;
 shipScale = 20;
+CreateAsset("Model", "Ship", 1545, 0, 1414, shipScale, shipScale, shipScale);
 
-CreateAsset("Model", "Cabin");
-cabinX, cabinY, cabinZ = 300, 50, 1200;
 cabinScale = 100;
-
-   nanosuit1X, nanosuit1Y, nanosuit1Z = 1900, 0, 1900;
-   nanosuit2X, nanosuit2Y, nanosuit2Z = 1700, 0, 2300;
-
-   pumpkin1X, pumpkin1Y, pumpkin1Z = 2234, 0, 1833;
-   pumpkin2X, pumpkin2Y, pumpkin2Z = 255, 0, 1007;
-   pumpkin3X, pumpkin3Y, pumpkin3Z = 1043, 0, 2660;
-   pumpkin4X, pumpkin4Y, pumpkin4Z = 1558, 0, 2772;
-   pumpkin5X, pumpkin5Y, pumpkin5Z = 1628, 0, 1189;
-   pumpkin6X, pumpkin6Y, pumpkin6Z = 580, 0, 2778;
-   pumpkin7X, pumpkin7Y, pumpkin7Z = 277, 0, 2515;
-   pumpkin8X, pumpkin8Y, pumpkin8Z = 1330, 0, 2158;
-   pumpkin9X, pumpkin9Y, pumpkin9Z = 276, 0, 1642;
-pumpkin10X, pumpkin10Y, pumpkin10Z = 270, 0, 359;
-pumpkin11X, pumpkin11Y, pumpkin11Z = 2125, 0, 1724;
-pumpkin12X, pumpkin12Y, pumpkin12Z = 2320, 0, 2302;
-pumpkin13X, pumpkin13Y, pumpkin13Z = 277, 0, 1477;
-pumpkin14X, pumpkin14Y, pumpkin14Z = 118, 0, 427;
-pumpkin15X, pumpkin15Y, pumpkin15Z = 1589, 0, 2115;
-pumpkin16X, pumpkin16Y, pumpkin16Z = 360, 0, 2176;
-pumpkin17X, pumpkin17Y, pumpkin17Z = 2617, 0, 1129;
-pumpkin18X, pumpkin18Y, pumpkin18Z = 242, 0, 2715;
-pumpkin19X, pumpkin19Y, pumpkin19Z = 209, 0, 774;
-pumpkin20X, pumpkin20Y, pumpkin20Z = 219, 0, 874;
-
-   barrel1X, barrel1Y, barrel1Z = 1116, 0, 1327;
-   barrel2X, barrel2Y, barrel2Z = 1289, 0, 1066;
-   barrel3X, barrel3Y, barrel3Z = 1325, 0, 1826;
-   barrel4X, barrel4Y, barrel4Z = 253, 0, 237;
-   barrel5X, barrel5Y, barrel5Z = 649, 0, 1589;
-   barrel6X, barrel6Y, barrel6Z = 731, 0, 2747;
-   barrel7X, barrel7Y, barrel7Z = 1194, 0, 1025;
-   barrel8X, barrel8Y, barrel8Z = 43, 0, 672;
-   barrel9X, barrel9Y, barrel9Z = 1363, 0, 1678;
-barrel10X, barrel10Y, barrel10Z = 955, 0, 2148;
-barrel11X, barrel11Y, barrel11Z = 608, 0, 2287;
-barrel12X, barrel12Y, barrel12Z = 101, 0, 803;
-barrel13X, barrel13Y, barrel13Z = 110, 0, 792;
-barrel14X, barrel14Y, barrel14Z = 717, 0, 1703;
-barrel15X, barrel15Y, barrel15Z = 231, 0, 928;
-barrel16X, barrel16Y, barrel16Z = 1590, 0, 1331;
-barrel17X, barrel17Y, barrel17Z = 199, 0, 501;
-barrel18X, barrel18Y, barrel18Z = 79, 0, 919;
-barrel19X, barrel19Y, barrel19Z = 1202, 0, 1652;
-barrel20X, barrel20Y, barrel20Z = 1431, 0, 1566;
-
-   crate1X, crate1Y, crate1Z = 1633, 0, 1555;
-   crate2X, crate2Y, crate2Z = 220, 0, 1613;
-   crate3X, crate3Y, crate3Z = 220, 0, 2470;
-   crate4X, crate4Y, crate4Z = 2290, 0, 622;
-   crate5X, crate5Y, crate5Z = 681, 0, 2505;
-   crate6X, crate6Y, crate6Z = 1626, 0, 1778;
-   crate7X, crate7Y, crate7Z = 831, 0, 2193;
-   crate8X, crate8Y, crate8Z = 1240, 0, 1871;
-   crate9X, crate9Y, crate9Z = 1359, 0, 962;
-crate10X, crate10Y, crate10Z = 1085, 0, 1347;
-crate11X, crate11Y, crate11Z = 2401, 0, 217;
-crate12X, crate12Y, crate12Z = 771, 0, 1333;
-crate13X, crate13Y, crate13Z = 1411, 0, 1539;
-crate14X, crate14Y, crate14Z = 1818, 0, 2552;
-crate15X, crate15Y, crate15Z = 2794, 0, 524;
-crate16X, crate16Y, crate16Z = 1706, 0, 1023;
-crate17X, crate17Y, crate17Z = 1217, 0, 2102;
-crate18X, crate18Y, crate18Z = 804, 0, 1014;
-crate19X, crate19Y, crate19Z = 1135, 0, 275;
-crate20X, crate20Y, crate20Z = 1708, 0, 1886;
+CreateAsset("Model", "Cabin", 300, 50, 1200, cabinScale, cabinScale, cabinScale);
--- Engine/src/Core/LuaRegistry.cpp
+++ Engine/src/Core/LuaRegistry.cpp
@@ -4,27 +4,84 @@ int LuaRegistry::Lua_Create(lua_State* L)
 {
 	int numParams = lua_gettop(L); //Number of parameters on the stack
 
-	if (numParams < 1 || numParams > 2) //Check we have enough parameters
+	if (numParams != 1 && numParams != 8 && numParams != 11) //Check we have enough parameters
 	{
-		Logger::LogError("Lua_Create: not enough parameters for Create function");
+		Logger::LogError("Lua_CreateAsset: not enough parameters for Create function");
 	}
 
-	if (!Lua::LuaType(L, -1, "string") || (numParams >= 2 && !Lua::LuaType(L, -2, "string"))) //Check all the parameters are strings
+	//Check all the parameters are correct
+	if ((numParams == 1 && !Lua::LuaType(L, -1, "string"))
+		|| (numParams == 8 && !Lua::LuaType(L, -8, "string") && !Lua::LuaType(L, -7, "string") && !Lua::LuaType(L, -6, "number") && !Lua::LuaType(L, -5, "number") && !Lua::LuaType(L, -4, "number") && !Lua::LuaType(L, -3, "number") && !Lua::LuaType(L, -2, "number") && !Lua::LuaType(L, -1, "number"))
+		|| (numParams == 11 && !Lua::LuaType(L, -8, "string") && !Lua::LuaType(L, -7, "string") && !Lua::LuaType(L, -6, "number") && !Lua::LuaType(L, -5, "number") && !Lua::LuaType(L, -4, "number") && !Lua::LuaType(L, -3, "number") && !Lua::LuaType(L, -2, "number") && !Lua::LuaType(L, -1, "number"))) //Check all the parameters are correct
 	{
-		Logger::LogError("Lua_Create: parameters are not valid!");
+		Logger::LogError("Lua_CreateAsset: parameters are not valid!");
 	}
 
 	std::string name;
 	std::string assetType;
 	InternalAsset* asset;
 
-	if (numParams == 2)
+	if (numParams == 11)
 	{
-		name = lua_tostring(L, -1); //Get the parameters from the stack
-		assetType = lua_tostring(L, -2);
+		//Get the parameters from the stack
+		float rotZ = lua_tonumber(L, -1);
+		float rotY = lua_tonumber(L, -2);
+		float rotX = lua_tonumber(L, -3);
+		float scaleZ = lua_tonumber(L, -4);
+		float scaleY = lua_tonumber(L, -5);
+		float scaleX = lua_tonumber(L, -6);
+		float posZ = lua_tonumber(L, -7);
+		float posY = lua_tonumber(L, -8);
+		float posX = lua_tonumber(L, -9);
+		name = lua_tostring(L, -10);
+		assetType = lua_tostring(L, -11);
+
 		lua_pop(L, -1);
 		lua_pop(L, -2);
+		lua_pop(L, -3);
+		lua_pop(L, -4);
+		lua_pop(L, -5);
+		lua_pop(L, -6);
+		lua_pop(L, -7);
+		lua_pop(L, -8);
+		lua_pop(L, -9);
+		lua_pop(L, -10);
+		lua_pop(L, -11);
+
 		asset = GameAssetFactory::Instance().Create(assetType, name); //Create Asset
+
+		((GameObject*)asset)->transform.SetPosition(posX, posY, posZ);
+
+		((GameObject*)asset)->transform.SetScale(scaleX, scaleY, scaleZ);
+
+		((GameObject*)asset)->transform.SetRotation(rotX, rotY, rotZ);
+	}
+	else if (numParams == 8)
+	{
+		//Get the parameters from the stack
+		float scaleZ = lua_tonumber(L, -1);
+		float scaleY = lua_tonumber(L, -2);
+		float scaleX = lua_tonumber(L, -3);
+		float posZ = lua_tonumber(L, -4);
+		float posY = lua_tonumber(L, -5);
+		float posX = lua_tonumber(L, -6);
+		name = lua_tostring(L, -7);
+		assetType = lua_tostring(L, -8);
+
+		lua_pop(L, -1);
+		lua_pop(L, -2);
+		lua_pop(L, -3);
+		lua_pop(L, -4);
+		lua_pop(L, -5);
+		lua_pop(L, -6);
+		lua_pop(L, -7);
+		lua_pop(L, -8);
+
+		asset = GameAssetFactory::Instance().Create(assetType, name); //Create Asset
+
+		((GameObject*)asset)->transform.SetPosition(posX, posY, posZ);
+
+		((GameObject*)asset)->transform.SetScale(scaleX, scaleY, scaleZ);
 	}
 	else
 	{
@@ -36,8 +93,8 @@ int LuaRegistry::Lua_Create(lua_State* L)
 		}
 		else
 		{
-			Logger::LogError("Lua_Create: Not enough parameters to create a ", assetType, "!");
-			throw "Lua_Create: Not enough parameters to create requested type!";
+			Logger::LogError("Lua_CreateAsset: Not enough parameters to create a ", assetType, "!");
+			throw "Lua_CreateAsset: Not enough parameters to create requested type!";
 		}
 	}
 
@@ -50,12 +107,14 @@ int LuaRegistry::Lua_CreateObject(lua_State* L)
 {
 	int numParams = lua_gettop(L); //Number of parameters on the stack
 
-	if (numParams != 5) //Check we have enough parameters
+	if (numParams != 7 && numParams != 10) //Check we have enough parameters
 	{
-		Logger::LogError("Lua_Create: not enough parameters for CreateObject function");
+		Logger::LogError("Lua_CreateObject: not enough parameters for CreateObject function");
 	}
 
-	if (!Lua::LuaType(L, -5, "string") && !Lua::LuaType(L, -4, "number") && !Lua::LuaType(L, -3, "number") && !Lua::LuaType(L, -2, "number") && !Lua::LuaType(L, -1, "number")) //Check all the parameters are correct
+	//Check all the parameters are correct
+	if ((numParams == 7 && !Lua::LuaType(L, -7, "string") && !Lua::LuaType(L, -6, "number") && !Lua::LuaType(L, -5, "number") && !Lua::LuaType(L, -4, "number") && !Lua::LuaType(L, -3, "number") && !Lua::LuaType(L, -2, "number") && !Lua::LuaType(L, -1, "number"))
+		|| (numParams == 10 && !Lua::LuaType(L, -10, "string") && !Lua::LuaType(L, -9, "number") && !Lua::LuaType(L, -8, "number") && !Lua::LuaType(L, -7, "number") && !Lua::LuaType(L, -6, "number") && !Lua::LuaType(L, -5, "number") && !Lua::LuaType(L, -4, "number") && !Lua::LuaType(L, -3, "number") && !Lua::LuaType(L, -2, "number") && !Lua::LuaType(L, -1, "number")))
 	{
 		Logger::LogError("Lua_CreateObject: parameters are not valid!");
 	}
@@ -64,28 +123,68 @@ int LuaRegistry::Lua_CreateObject(lua_State* L)
 	std::string assetType;
 	InternalAsset* asset;
 
-	if (numParams == 5)
+	if (numParams == 10)
 	{
-		float scale = lua_tonumber(L, -1);
-		float z = lua_tonumber(L, -2);
-		float y = lua_tonumber(L, -3);
-		float x = lua_tonumber(L, -4);
-		assetType = lua_tostring(L, -5);
+		//Get the parameters from the stack
+		float rotZ = lua_tonumber(L, -1);
+		float rotY = lua_tonumber(L, -2);
+		float rotX = lua_tonumber(L, -3);
+		float scaleZ = lua_tonumber(L, -4);
+		float scaleY = lua_tonumber(L, -5);
+		float scaleX = lua_tonumber(L, -6);
+		float posZ = lua_tonumber(L, -7);
+		float posY = lua_tonumber(L, -8);
+		float posX = lua_tonumber(L, -9);
+		assetType = lua_tostring(L, -10);
+
 		lua_pop(L, -1);
 		lua_pop(L, -2);
 		lua_pop(L, -3);
 		lua_pop(L, -4);
 		lua_pop(L, -5);
+		lua_pop(L, -6);
+		lua_pop(L, -7);
+		lua_pop(L, -8);
+		lua_pop(L, -9);
+		lua_pop(L, -10);
+
+		asset = GameAssetFactory::Instance().Create(assetType); //Create Asset
+
+		((GameObject*)asset)->transform.SetPosition(posX, posY, posZ);
+
+		((GameObject*)asset)->transform.SetScale(scaleX, scaleY, scaleZ);
+
+		((GameObject*)asset)->transform.SetRotation(rotX, rotY, rotZ);
+	}
+	else if (numParams == 7)
+	{
+		//Get the parameters from the stack
+		float scaleZ = lua_tonumber(L, -1);
+		float scaleY = lua_tonumber(L, -2);
+		float scaleX = lua_tonumber(L, -3);
+		float posZ = lua_tonumber(L, -4);
+		float posY = lua_tonumber(L, -5);
+		float posX = lua_tonumber(L, -6);
+		assetType = lua_tostring(L, -7);
+
+		lua_pop(L, -1);
+		lua_pop(L, -2);
+		lua_pop(L, -3);
+		lua_pop(L, -4);
+		lua_pop(L, -5);
+		lua_pop(L, -6);
+		lua_pop(L, -7);
+
 		asset = GameAssetFactory::Instance().Create(assetType); //Create Asset
 		
-		((GameObject*)asset)->transform.SetPosition(x, y, z);
+		((GameObject*)asset)->transform.SetPosition(posX, posY, posZ);
 
-		((GameObject*)asset)->transform.SetScale(scale, scale, scale);
+		((GameObject*)asset)->transform.SetScale(scaleX, scaleY, scaleZ);
 	}
 	else
 	{
 		Logger::LogError("Lua_Create: Not enough parameters to create a ", assetType, "!");
-		throw "Lua_Create: Not enough parameters to create requested type!";
+		throw "Lua_CreateObject: Not enough parameters to create requested type!";
 	}
 
 	Lua::AddCreatedAsset(asset);
